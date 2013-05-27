27447c627e5c1e745b1689152a4ae56f219e1da8
Nattapong Tonprasert <luciferultram@gmail.com>
2013-05-27 10:40:59 +0700

    use menu

--- HelloWeather/gen/com/utk/helloweather/R.java
+++ HelloWeather/gen/com/utk/helloweather/R.java
@@ -24,6 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int action_hello=0x7f080004;
+        public static final int action_refresh=0x7f080003;
         public static final int action_settings=0x7f080002;
         public static final int listView=0x7f080001;
         public static final int text_search=0x7f080000;
